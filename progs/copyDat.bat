84df45c239c9567dc5803036b41ce7090d714965
Ben <van.citters.b@gmail.com>
2021-11-01 19:48:14 -0500

    fixing the batch file for this project

--- progs/copyDat.bat
+++ progs/copyDat.bat
@@ -1,4 +1,8 @@
-copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\mpmod"\progs.dat
-copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat
+rem copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\mpmod"\progs.dat
+rem copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat
+
+copy "C:\Users\vanci\Documents\myMods\New folder\progs.dat" "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat /y
+copy "C:\Users\vanci\Documents\myMods\New folder\progs.dat" "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\mpmod" /y
+
 timeout /T 2
 "c:\Program Files (x86)\Steam\steamapps\common\Quake\rerelease\Quake_x64_steam.exe"
